252f7e1c27b0875776138733ba41ead1ddf9614a
Meruert <mika.amantaikuzu@gmail.com>
2024-10-20 14:52:55 +0500

    feat: implement user create func with uuid

---
+++ src/create-user.ts
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
+
+interface User {
+  id: string;
+  username: string;
+  age: number;
+  hobbies: string[];
+}
+
+const users: User[] = [];
+
+const createUser = (username: string, age: number, hobbies: string[]): User => {
+  const newUser: User = {
+    id: uuidv4(),
+    username,
+    age,
+    hobbies
+  };
+  users.push(newUser);
+  return newUser;
+};
+
