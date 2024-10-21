4d3d0a2ee0a3cebe9f4391de3a0e0ceeea3a6e34
Meruert <mika.amantaikuzu@gmail.com>
2024-10-21 11:31:05 +0500

    feat: not exist request

--- app.ts
+++ app.ts
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
   const url = request.url?.split("/"); // [ '', 'api', 'users' ]
 
+  const existMethods = ["GET", "POST", "PUT", "DELETE"];
+
+  function isExistMethod(request: string) {
+    return existMethods.includes(request);
+  }
+
   if (request.method === "GET") {
     if (url?.length === 3) {
       getUsers(response);
@@ -32,6 +38,11 @@ const server = http.createServer((request: http.IncomingMessage, response: http.
     if (url?.length === 4) {
       deleteUser(url[3], response);
     }
+  }
+
+  if (request.method && !isExistMethod(request.method)) {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: "Oops.. your request doesn't exist or not supported, try again" }));
   }
 });
 
