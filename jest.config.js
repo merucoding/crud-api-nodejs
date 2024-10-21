3592c9f29d457ad139f433d21486d23d894a2c14
Meruert <mika.amantaikuzu@gmail.com>
2024-10-21 20:28:17 +0500

    feat: complete tests

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/?(*.)+(test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  restoreMocks: true,
+  resetMocks: true,
+  moduleDirectories: ['node_modules', '<rootDir>/test'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,27 @@
-// Получить все записи с помощью GET api/users запроса (ожидается пустой массив)
-// Новый объект создается запросом POST api/users(ожидается ответ, содержащий вновь созданную запись)
-// С помощью GET api/user/{userId}запроса мы пытаемся получить созданную запись по ее id(ожидается созданная запись)
+import request from "supertest";
+import { server } from "../src/app";
 
-// import request from "supertest";
-// import { server } from "../src/app.ts";
+const user = { username: "anna", age: 25, hobbies: [] };
 
-// describe("api tests", () => {
-//   test("should return users with a GET request (an empty array)", async () => {
-//     const response = await request(server).get('/api/users');
-//     expect(response.status).toEqual(200);
-//     expect(response.body).toEqual([]);
-//   });
+describe("api tests", () => {
+  test("should return users with a GET request (an empty array)", async () => {
+    const response = await request(server).get("/api/users");
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual([]);
+  });
 
-//   test("", () => {});
+  test("should create a new user with a POST request", async () => {
+    const res = await request(server).post("/api/users").send(user).set("Content-Type", "application/json");
+    expect(res.status).toBe(201);
+    console.log(res.body);
+  });
 
-//   test("", () => {});
-// });
+  test("get created user by id with GET request", async () => {
+    const post = await request(server).post("/api/users").send(user).set("Content-Type", "application/json");
+    expect(post.status).toBe(201);
+
+    const res = await request(server).get(`/api/users/${post.body.id}`);
+    expect(res.status).toBe(200);
+    console.log(res.body);
+  });
+});
